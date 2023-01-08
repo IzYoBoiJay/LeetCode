31c1b319a04e0a56e8a96e71201a57a9f1cde6fc
JT Calahatian <55920071+IzYoBoiJay@users.noreply.github.com>
2023-01-08 10:15:48 -0500

    Time: 496 ms (76.18%), Space: 15.2 MB (80.26%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,16 +1,20 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         
+        #To keep track of the furthest index able to reach
         maxIndex = 0
         
-        for i, num in enumerate(nums):
+        #Iterate through each element in nums
+        for i, jumpNum in enumerate(nums):
             
+            #Reached an index that cannot be reached (further than the maxIndex possible)
             if i > maxIndex:
                 
                 return False
             
-            maxIndex = max(maxIndex, i + num)
+            #Compare the current maxIndex to the current index plus its jump to determine a new maxIndex
+            maxIndex = max(maxIndex, i + jumpNum)
 
-            
+        #Able to reach the end as the condition where the maxIndex and the last index is the same
         return True
             
